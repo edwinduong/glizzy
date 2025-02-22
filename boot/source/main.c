73e20a1dbbb55be1760a015b7f95307199c4bb78
edwinduong <69070602+edwinduong@users.noreply.github.com>
2025-02-22 03:18:07 -0600

    Refactor into smaller functions

--- boot/source/main.c
+++ boot/source/main.c
@@ -1,13 +1,12 @@
-//main.c
+// main.c
+// AUTHORS: Edwin Duong, Mark Lysack
 
-/*
-AUTHORS: Edwin Duong, Mark Lysacks
-*/
-
+// Imports
 #include <stdlib.h>
 #include <wiiuse/wpad.h>
 #include <grrlib.h>
 
+// Images
 #include "BMfont1_png.h"
 #include "BMfont2_png.h"
 #include "BMfont3_png.h"
@@ -41,11 +40,18 @@ AUTHORS: Edwin Duong, Mark Lysacks
 #define GRRLIB_AQUA    0x00FFFFFF
 #define GRRLIB_WHITE   0xFFFFFFFF
 
-// Thresholds
-#define TOP 12200
-#define BOT 4200
+// Movement thresholds
+#define TOP_THRESH 12200
+#define BOT_THRESH 4200
 
+// Gameplay settings
 #define NUM_PLAYERS 2
+
+// Draw constants
+const s32 LEFT = 0;
+const s32 TOP = 0;
+const s32 RIGHT = 640;
+const s32 BOTTOM = 480;
 
 typedef struct Goblin {
     int glizzies;
@@ -53,11 +59,11 @@ typedef struct Goblin {
     int bottomed;
 } Goblin;
 
+// Game state
 int gaming = 0;
-s32 left = 0, top = 0, right = 640, bottom = 480;
-
 Goblin goblin[NUM_PLAYERS];
 
+// Textures
 GRRLIB_texImg *tex_BMfont1;
 GRRLIB_texImg *tex_BMfont2;
 GRRLIB_texImg *tex_BMfont3;
@@ -76,12 +82,12 @@ GRRLIB_texImg *tex_table_4;
 void title() {
     const u32 wpadheld = WPAD_ButtonsHeld(0);
 
-    GRRLIB_DrawImg((640 - 480) / 2, top, tex_title, 0, 2, 1.9, GRRLIB_WHITE);
+    GRRLIB_DrawImg((640 - 480) / 2, TOP, tex_title, 0, 2, 1.9, GRRLIB_WHITE);
     GRRLIB_Printf((640 - 368) / 2, 480 - 16 - 4, tex_BMfont2, GRRLIB_GREEN, 1, "PRESS A AND B TO START");
     // 23 chars
 
     if((wpadheld & WPAD_BUTTON_A) && (wpadheld & WPAD_BUTTON_B)) {
-        WPAD_SetMotionPlus(-1, 1);
+        WPAD_SetMotionPlus(WPAD_CHAN_ALL, 1);
         gaming = 1;
     }
 }
@@ -92,8 +98,8 @@ void game() {
         short p = wd->exp.mp.rx;
 
         GRRLIB_texImg *tex_goblin = tex_goblin_4;
-        if(goblin[i].bottomed && p < BOT) tex_goblin = tex_goblin_1;
-        if(goblin[i].bottomed && p > BOT) tex_goblin = tex_goblin_2;
+        if(goblin[i].bottomed && p < BOT_THRESH) tex_goblin = tex_goblin_1;
+        if(goblin[i].bottomed && p > BOT_THRESH) tex_goblin = tex_goblin_2;
         if(goblin[i].topped) tex_goblin = tex_goblin_3;
 
         GRRLIB_texImg *tex_table = tex_table_1;
@@ -105,12 +111,12 @@ void game() {
             tex_table = tex_table_2;
         }
 
-        GRRLIB_DrawImg(left + (right / (i * NUM_PLAYERS)), top, tex_goblin, 0, 1, 1, GRRLIB_WHITE);
-        GRRLIB_DrawImg(left + (right / (i * NUM_PLAYERS)), top, tex_table, 0, 1, 1, GRRLIB_WHITE);
+        GRRLIB_DrawImg(LEFT + (i * (RIGHT / NUM_PLAYERS)), TOP, tex_goblin, 0, 1, 1, GRRLIB_WHITE);
+        GRRLIB_DrawImg(LEFT + (i * (RIGHT / NUM_PLAYERS)), TOP, tex_table, 0, 1, 1, GRRLIB_WHITE);
 
-        if(p < BOT) goblin[i].bottomed = 1;
-        if(goblin[i].bottomed == 1 && p > TOP) goblin[i].topped = 1;
-        if(goblin[i].topped == 1 && goblin[i].bottomed == 1 && p > BOT && p < TOP) {
+        if(p < BOT_THRESH) goblin[i].bottomed = 1;
+        if(goblin[i].bottomed == 1 && p > TOP_THRESH) goblin[i].topped = 1;
+        if(goblin[i].topped == 1 && goblin[i].bottomed == 1 && p > BOT_THRESH && p < TOP_THRESH) {
             goblin[i].bottomed = 0;
             goblin[i].topped = 0;
             goblin[i].glizzies++;
@@ -118,63 +124,17 @@ void game() {
     }
 }
 
-void load_textures();
-
-void free_textures();
-
-int main() {
-    GRRLIB_Init();
-
-    WPAD_Init();
-    WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
-    WPAD_SetDataFormat(WPAD_CHAN_1, WPAD_FMT_BTNS_ACC_IR);
-
-    load_textures();
-
-    while(1) {
-        WPAD_SetVRes(0, 640, 480);
-        WPAD_ScanPads();
-        const u32 wpaddown = WPAD_ButtonsDown(0);
-
-        GRRLIB_FillScreen(GRRLIB_WHITE);
-        WPAD_Rumble(WPAD_CHAN_0, 0);
-        WPAD_Rumble(WPAD_CHAN_1, 0);
-
-        if(gaming)
-            game();
-        else
-            title();
-
-        if(wpaddown & WPAD_BUTTON_HOME) {
-            break;
-        }
-
-        GRRLIB_Render();
-    }
-
-    free_textures();
-
-    GRRLIB_Exit();
-
-    return 0;
-}
-
 void load_textures() {
     tex_BMfont1 = GRRLIB_LoadTexture(BMfont1_png);
     GRRLIB_InitTileSet(tex_BMfont1, 32, 32, 32);
-
     tex_BMfont2 = GRRLIB_LoadTexture(BMfont2_png);
     GRRLIB_InitTileSet(tex_BMfont2, 16, 16, 32);
-
     tex_BMfont3 = GRRLIB_LoadTexture(BMfont3_png);
     GRRLIB_InitTileSet(tex_BMfont3, 32, 32, 32);
-
     tex_BMfont4 = GRRLIB_LoadTexture(BMfont4_png);
     GRRLIB_InitTileSet(tex_BMfont4, 16, 16, 32);
-
     tex_BMfont5 = GRRLIB_LoadTexture(BMfont5_png);
     GRRLIB_InitTileSet(tex_BMfont5, 8, 16, 0);
-
     tex_title = GRRLIB_LoadTexture(title_jpg_jpg);
     tex_goblin_1 = GRRLIB_LoadTexture(goblin_1_jpg);
     tex_goblin_2 = GRRLIB_LoadTexture(goblin_2_jpg);
@@ -193,4 +153,44 @@ void free_textures() {
     GRRLIB_FreeTexture(tex_BMfont3);
     GRRLIB_FreeTexture(tex_BMfont4);
     GRRLIB_FreeTexture(tex_BMfont5);
+}
+
+void setup() {
+    GRRLIB_Init();
+    WPAD_Init();
+    WPAD_SetDataFormat(WPAD_CHAN_ALL, WPAD_FMT_BTNS_ACC_IR);
+    load_textures();
+}
+
+void teardown() {
+    free_textures();
+    GRRLIB_Exit();
+}
+
+void draw() {
+    while(1) {
+        WPAD_SetVRes(0, 640, 480);
+        WPAD_ScanPads();
+        const u32 wpaddown = WPAD_ButtonsDown(0);
+
+        GRRLIB_FillScreen(GRRLIB_WHITE);
+        WPAD_Rumble(WPAD_CHAN_ALL, 0);
+
+        if(gaming)
+            game();
+        else
+            title();
+
+        if(wpaddown & WPAD_BUTTON_HOME)
+            break;
+
+        GRRLIB_Render();
+    }
+}
+
+int main() {
+    setup();
+    draw();
+    teardown();
+    return 0;
 }
